7db1f5d2fc20dfe194af257710ae6065d8e87448
Piotr <a@b.c>
2019-07-19 10:57:28 +0200

    Day four - pasby

---
+++ pasby/scripts/main.py
@@ -0,0 +1,10 @@
+def to_upper_case(string):
+    string = "HELLO WORLD"  # string.upper()
+
+
+if __name__ == '__main__':
+    s = "Hello World"
+    to_upper_case(s)
+    print(s)
+    print(id(s))
+
